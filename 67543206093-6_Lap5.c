3e50fffbbccd5a20fc735bec2a9c5582756bb142
Natdanai804 <nongneno2549@gmail.com>
2024-10-19 17:35:53 +0700

    Update 67543206093-6_Lap5.c

--- 67543206093-6_Lap5.c
+++ 67543206093-6_Lap5.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
-int main() {
-    int score ;
-    printf(" put your score:") ;
-    scanf("%d",&score) ; 
 
-    if(score >= 0 && score <= 49) 
-        printf("You are grade F \n") ;    
-    else if((score >= 50 && score <= 55))
-        printf("You are grade D\n") ;
-    else if((score >= 55 && score <= 60))
-        printf("You are grade D+\n") ;
-    else if((score >= 60 && score <= 65))
-        printf("You are grade C\n") ;
-    else if((score >= 65 && score <= 70))
-        printf("You are grade C+\n") ;
-    else if((score >= 70 && score <= 75))
-        printf("You are grade B\n") ;
-    else if((score >= 75 && score <= 80))
-        printf("You are grade B+\n") ;
-    else if((score <= 80))  
-        printf("You are grade A\n") ;
-    else 
-        printf("Your score is incorrect") ;
+int main() { //main function
 
+    //--| YOUR CODE HERE
+    int score ; // Score
+    char input[ 10 ] ;
+    printf( "Enter score: " ) ;
+    fgets( input, sizeof( input ) , stdin ) ;  // read the input as a string
+
+    if ( sscanf(input , "%d" , &score ) != 1 ) { // convert the string input to an integer
+        printf( "please enter number only.\n" ) ;
+    } else if ( score >= 80 ) { // Start Checking Grade
+        printf( "A !\n" ) ;
+    } else if ( score >= 75 ) {
+        printf( "B+ !\n" ) ;
+    } else if ( score >= 70 ) {
+        printf( "B !\n" ) ;
+    } else if ( score >= 65 ) {
+        printf( "C+ !\n" ) ;
+    } else if ( score >= 60 ) {
+        printf( "C !\n" ) ;
+    } else if ( score >= 55 ) {
+        printf( "D+ !\n" ) ;
+    } else if ( score >= 50 ) {
+        printf( "D !\n" ) ;
+    } else {
+        printf( "F !\n" ) ;
+    }//end if-else
+     
     return 0 ;
-}//end main function
