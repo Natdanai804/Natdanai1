7c4abedc8f3cac89e155ea1ec2dee8705ed624b0
Natdanai804 <nongneno2549@gmail.com>
2024-10-19 17:33:58 +0700

    Update 67543206093-6_Lab1.c

--- 67543206093-6_Lab1.c
+++ 67543206093-6_Lab1.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    char Name[50];
-    int Age = 0;
+    char Name[ 50 ] ;
+    int  Age = 0 ;
+    printf( "Enter your name: " ) ;
+    scanf( "%s" , Name ) ;
+    printf( "Enter your age: " ) ;
+    scanf( "%d" , &Age ) ;
+    printf( "- - - - - -\n" ) ;
+    printf( "Hello %s \n" , Name ) ; //TODO: #14 Say hello to user.
+    printf( "Age = %d\n" , Age ) ; //TODO: #15 display user their age number plus 1.
     
-   
-    printf("Enter your name: ");
-    scanf("%s", Name);
-    
-    
-    printf("Enter your age: ");
-    scanf("%d", &Age);
-    
-    
-    printf("- - - - - -\n");
-    
-  
-    printf("Hello %s\n", Name); 
-    
-    
-    printf("Age = %d\n", Age );
-    
-    return 0;
-}//end main
+}//end main function
