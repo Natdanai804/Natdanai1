85ecdab2bc644dc24e359708f71286899daeb63c
Natdanai804 <nongneno2549@gmail.com>
2024-10-19 17:35:10 +0700

    Update 67543206093-6_Lap4.c

--- 67543206093-6_Lap4.c
+++ 67543206093-6_Lap4.c
@@ -1,23 +1,18 @@
-
 #include <stdio.h>
 
 int main() {
+    char ID[ 10 ] ;
+    int WorkingHour, Salary ;
+    printf( "\n\nInput the Employees ID (Max. 10 chars): " ) ;
+    scanf( "%s", ID ) ;
+    printf( "Input the working hrs: " ) ;
+    scanf( "%d", &WorkingHour ) ;
+    printf( "Salary amount/hr: " ) ;
+    scanf( "%d", &Salary ) ;
 
-    char Employees_ID[10] ;
-    int WorkingHour ;
-    int Salary ;
-    
-    printf( "Input the Employees ID(Max. 10 chars):" ) ;
-    scanf("%s", Employees_ID ) ;
-    printf("Input the working hr: " ) ;
-    scanf("%d", &WorkingHour ) ;
-    printf("Salary amount/hr: " ) ;
-    scanf("%d", &Salary ) ;
-    
-    printf( "\n\n" ) ;
-    printf( "Employees ID = %s", Employees_ID ) ;
-    printf( "/nsalary = %d THB", WorkingHour * 30 * Salary ) ;
-    printf( "\n\n" ) ;
-
+    printf( "Employees ID = %s\n", ID ) ;
+    printf("Salary = U$ %.2f\n\n", (float)(WorkingHour * Salary ));
+   
     return 0 ;
-}//end main function
+}//end function 
+ 
