4383abc39afdfdd670cf079ed044e77cb2ec6a97
Natdanai804 <nongneno2549@gmail.com>
2024-10-19 17:37:27 +0700

    Update 67543206093-6_Lap7.c

--- 67543206093-6_Lap7.c
+++ 67543206093-6_Lap7.c
@@ -1,49 +1,65 @@
-#include <stdio.h>      
-#include <stdlib.h>     
-#include <time.h>       
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 int main() {
-    void playGame() {
-    int r ;                                      //r = ตัวแรมดอมตัวเลขในแต่ละครั้ง  
-    int number ;                                 //number = ค่าที่ผู้ใช้กรอก
-    r = rand() % 100 + 1 ;                       //การ random ตัวเลขตั้งแต่ 0 - 99 + 1
-    int lower = 1 , high = 100 ;                 // เลขต่ำสุด , เลขสูงสุด
-    int score = 100 ;                            // คะแนน
-    printf("(player Score=%d)\n", score) ;       //แสดงค่าคะแนนเริ่มต้น  =  100
-    while (1) {   
-        printf("Guess the winning number (%d-%d) :\n", lower , high ) ;    //  ให้เดาหมายเลขที่ชนะตั้งแต่ 1 - 100
-        scanf("%d", & number ) ;                                           //  เก็บตัวเลขที่ผู้ใช้เดามาไว้ในตัวแปร number  
-        if (number == r ) {                                                // ถ้าค่าที่ผู้ใช้กรอก = ค่าที่โปรแกรมแรนดอม
-            printf("That is correct! The winning number is %d.\n", r ) ;   // แสดงว่า ถูกต้อง! หมายเลขที่ชนะคือ ...
-            printf("Score this game: %d\n", score) ;                       // แสดงคะแนนที่เหลือของผู้เล่น  
-            break ;   
-        } else {   
-            score -= 10 ;                           //ถ้าตอบผิดลบทีละ10คะแนน
-            if (score < 0) {                        //ถ้าคะแนน < 0    
-                score = 0 ;                         //ถ้าคะแนน = 0  
-                printf(" you lost\n ") ;            //แสดงว่า คุณแพ้
-                break ;                             //ให้หยุดละเริ่มใหม่
-            }
-            if (number < r ) {                               //ถ้า ค่าที่ผู้ใช้กรอก < ค่าที่โปรแกรมแรนดอม
-                printf("Sorry, the winning number is HIGHER than %d. (Score=%d)\n", number , score) ;   //แสดงว่า ขออภัยหมายเลขที่ชนะสูงกว่าค่าที่ผู้ใช้กรอก 
-                lower = number + 1 ;                         // lower = ค่าที่ผู้ใช้กรอก + 1
-            } else {   
-                printf("Sorry, the winning number is LOWER than %d. (Score=%d)\n", number , score) ;    //แสดง  ขออภัยหมายเลขที่ชนะต่ำกว่าค่าที่ผู้ใช้กรอก
-                high  = number - 1 ;                         // high  = ค่าที่ผู้ใ้ชกรอก - 1
-            }
+    int play = 0 ; 
+    int guess, answer, score ;
+    int min = 1 , max = 100 ;
+
+    srand(time(NULL));
+
+    do {
+        printf("Do you want to play game (1=play, -1=exit) : ") ;
+        scanf("%d", &play) ;
+
+        if (play == 1) {
+            score = 100 ;
+            answer = 42 ; 
+
+            printf("(Score=%d)\n" , score);
+
+            do {
+                
+                printf("Guess the winning number (%d-%d): ", min, max) ;
+                scanf("%d" , &guess) ;
+
+                if (guess == answer) {
+                    printf("That is correct! The winning number is %d.\n" , answer) ;
+                    printf("Score this game: %d\n" , score) ;
+                    break; 
+                } else if ( guess > max ){
+                    printf( "Your guess is out of the current bounds (%d-%d)! Try again.\n" , min , max ) ;
+                } else if ( min > guess ){
+                    printf( "Your guess is out of the current bounds (%d-%d)! Try again.\n" ,min , max ) ;
+                } else {
+                    score -= 10 ; 
+
+                    if (guess > answer) {
+                        printf("Sorry, the winning number is LOWER than %d. (Score=%d)\n" , guess , score) ;
+                        if ( guess > max ) {
+                        max = max ;
+                        } else {
+                        max = guess - 1 ;
+                        } // end if else
+                    } else {
+                        printf("Sorry, the winning number is HIGHER than %d. (Score=%d)\n" , guess , score) ;
+                        if ( guess < min ) {
+                        min = min ;
+                        } else {
+                        min = guess + 1 ;
+                        } // end if else
+                    }
+
+                    if (score == 0) {
+                        printf("Game over! You've run out of score.\n") ;
+                        break ;
+                    }
+                }
+            } while (guess != answer) ; 
         }
-    }
-}
-    int command ;       //ตัวแปรเพื่อรับค่าที่ผู้ใช้กรอกก่อนจะเข้าเกม
-    srand(time(NULL)) ;   //ใช้กำหนด randomseed ให้กับการทำงานของฟังก์ชั่น rand
-    while (1) {   
-        printf("Do you want to play game (1=play,-1=exit) :\n") ;    //คำถามก่อนจะเล่นเกม "คุณต้องการเล่นเกมมั้ย ถ้าเล่นกด1 ถ้าไม่เล่นกด-1"
-        scanf("%d", &command) ;    //สแกนค่าที่ผู้ใช้กรอกไว้ที่ command
-        if (command == 1) {             //ถ้าค่าที่ผู้ใช้กรอกก่อนจะเข้าเกม = 1
-            playGame () ;               //ทำการเริ่มทำงานเกม
-        } else if (command == -1) {     //ถ้าค่าที่ผู้ใช้กรอกก่อนจะเข้าเกม = -1
-            break ;                     //ทำการหยุดการทำงานของเกม
-    }  
-}
- 
+
+    } while (play != -1) ;
     return 0 ;
-}//end main function
+}
+
